2d47e2ea9e46ce07354c770020621919fe6e6f82
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-11-07 17:22:47 -0500

    Added Card, Contact, Container folders

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,31 @@
+// import React from "react";
+// import "./contact.css";
+
+// function Contact() {
+//     return (
+//         <div className="contact">
+//             <section className="section-3">
+//             <div className="container-fluid font" id="contactSection">
+//             <h2 id="sampleWork" className="sampleProjectsSection pt-5">Contact Me</h2>
+
+//             <form className="d-flex flex-column px-4" action="mailto:noha_ashraf85@hotmail.com?subject=contact me&amp;" method="post" encType="text/plain">
+//             Name<br>
+//             <input className="form-control" type="text" name="name" /><br>
+//             Email<br>
+//             <input className="form-control" type="text" name="mail" /><br>
+//             Message<br>
+//             <textarea className="form-control" type="text" name="message" size="50"></textarea><br /><br />
+//             <input className="btn btn-dark mb-2" type="submit" value="Send" />
+//             <input className="btn btn-dark mb-2" type="reset" value="Reset" />
+//             </form>
+
+//             </div>
+//             </div>
+//   </section>
+
+//         </div>
+
+//     );
+// }
+
+// export default Contact;
